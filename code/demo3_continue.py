1e08505d1350a6a1b1c377240402804def61bb33
jm <jm@hw4.16>
2024-03-21 19:23:49 +0800

    verify72 stage1

--- code/demo3_continue.py
+++ code/demo3_continue.py
@@ -34,7 +34,7 @@ model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['ac
 print(4)
 batch_size = 128
 # 继续训练
-epochs = 150  # 假设原来总共打算训练50个epoch
+epochs = 250  # 假设原来总共打算训练50个epoch
 last_epoch = int(latest_model_path.split('_')[-1].split('.')[0])  # 获取已训练的最后一个epoch号
 
 remaining_epochs = epochs - last_epoch  # 计算剩余需要训练的epoch数
@@ -46,7 +46,7 @@ model.fit_generator(
     steps_per_epoch=generator.get_num_samples() / batch_size,
     validation_data=test_generator.generate(batch_size, num_classes),
     validation_steps=test_generator.get_num_samples() / batch_size,
-    callbacks=[ModelCheckpoint('./checkpoints/large_model_epoch_{epoch}.h5')]
+    callbacks=[ModelCheckpoint('./cks/large_model_epoch_{epoch}.h5')]
 )
 print(5)
 
